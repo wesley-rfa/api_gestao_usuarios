81c7779408636d2b5add94dde96e552adb1b149a
vivicrisx <vivicrisx@gmail.com>
2023-06-11 17:05:51 -0300

    Criar método store e update para pedidos #26

---
+++ app/Http/Requests/UpdateOrderRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            "clienteId" => "integer",
+            "enderecoClienteId" => "integer",
+            "valor" => "",
+            "itens" => "array",
+            "itens.*.id" => "integer",
+            "itens.*.pratoId" => "integer",
+            "itens.*.quantidade" => "integer",
+        ];
+    }
+}
